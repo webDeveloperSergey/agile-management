e28e8f3a75fd004b228e63b77570731c7059ae59
Sergei <kultyshkin2003@gmail.com>
2026-04-19 11:58:15 +0300

    fix(main): не там cli создал папку с boards

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common'
 import { CoreModule } from './core/core.module'
 import { UsersModule } from './modules/users/users.module'
-import { AuthModule } from './modules/auth/auth.module';
-import { BoardsModule } from './boards/boards.module';
+import { AuthModule } from './modules/auth/auth.module'
+import { BoardsModule } from './modules/boards/boards.module'
 
 @Module({
   imports: [CoreModule, UsersModule, AuthModule, BoardsModule],
