78f2c6daed98f4ee791c3e02a24dc6e0a9d13582
Sergei <kultyshkin2003@gmail.com>
2026-05-03 16:07:39 +0300

    feat(main): документация и базовый инит сущности columns

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { CoreModule } from './core/core.module'
 import { UsersModule } from './modules/users/users.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { BoardsModule } from './modules/boards/boards.module'
+import { ColumnsModule } from './modules/columns/columns.module';
 
 @Module({
-  imports: [CoreModule, UsersModule, AuthModule, BoardsModule],
+  imports: [CoreModule, UsersModule, AuthModule, BoardsModule, ColumnsModule],
 })
 export class AppModule {}
